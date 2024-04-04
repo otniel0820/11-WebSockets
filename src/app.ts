0fed670f567f7140d3c4b3e27f44ddcf254e4f8d
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-04-04 23:30:24 +0200

    listenen client message server

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,26 @@ const wss = new WebSocketServer({ port: 3000 });
 
 wss.on('connection', function connection(ws) {
 
+  console.log(ws);
+  
   console.log('Client connected');
   
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+    console.log('Desde el cliente', data);
+    const payload = {
+      type: 'custom-message',
+      payload: data.toString()
+    }
+    ws.send(JSON.stringify(payload));
   });
 
-  ws.send('Hola desde el servidor');
+  // ws.send('Hola desde el servidor');
 
-  setInterval(() => {
-    ws.send('Hola de nuevo')
-  },2000)
+  ws.on('close', function close() {
+    console.log('Client disconnected');
+  })
 });
 
 console.log('Server running on port http://localhost:3000');
