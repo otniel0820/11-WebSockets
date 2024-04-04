e75e71efc8de1ba1858367619d54abf5a26d880f
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-04-04 23:37:58 +0200

    server broadcast

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
@@ -11,12 +11,19 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('Desde el cliente', data);
-    const payload = {
+    
+
+    const payload = JSON.stringify({
       type: 'custom-message',
       payload: data.toString()
-    }
-    ws.send(JSON.stringify(payload));
+    }) 
+    // ws.send(JSON.stringify(payload));
+
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(payload, { binary: false });
+      }
+    });
   });
 
   // ws.send('Hola desde el servidor');
